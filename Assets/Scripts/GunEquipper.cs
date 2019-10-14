26df95f2fcd3541ed732e0375a4d7d20e2cbb68b
sharmaiser <54967627+sharmaiser@users.noreply.github.com>
2019-10-14 19:58:25 -0400

    Chapter 9 Animations Added
    * Pistol: new components
    * ARiffle: new componets
    * Shotgun: new components

--- Assets/Scripts/GunEquipper.cs
+++ Assets/Scripts/GunEquipper.cs
@@ -32,6 +32,9 @@ public class GunEquipper : MonoBehaviour
         {
             loadWeapon(pistol);
             activeWeaponType = Constants.Pistol;
+        }
+        else if(Input.GetKeyDown("2"))
+        {
             loadWeapon(assaultRifle);
             activeWeaponType = Constants.AssaultRifle;
         }
