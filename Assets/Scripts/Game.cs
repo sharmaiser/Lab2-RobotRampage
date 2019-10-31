a5ac4bba14cac20d0c266dced5ed842d9b224361
sharmaiser <54967627+sharmaiser@users.noreply.github.com>
2019-10-31 19:27:43 -0400

    Chapter 10 Robot Spawns

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;
+    public int enemiesLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
